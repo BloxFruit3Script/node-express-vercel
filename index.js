657d81ee3d78e8873cdf6953c5408218c2e2e14e
TripleScript <secondtimelast28@gmail.com>
2024-08-26 16:10:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const fluxus = require("./routes/fluxus");
 const delta = require("./routes/delta");
 const relz = require("./routes/relz");
-const supported = require("./routes/supported");
 
 // Middlewares
 const app = express();
@@ -13,7 +12,6 @@ app.use(express.json());
 app.use("/fluxus", fluxus);
 app.use("/delta", delta);
 app.use("/relz", relz);
-app.use("/supported", supported);
 
 // connection
 const port = process.env.PORT || 9001;
