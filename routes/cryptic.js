d6b6a6877a9af8802f30ebf06039b7f88b6298a5
TripleScript <secondtimelast28@gmail.com>
2024-09-01 19:05:45 +0800

    Update and rename delta.js to cryptic.js

--- routes/cryptic.js
+++ routes/cryptic.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    message: "Api is currently private for now!",
+    sta:true
+    bypassed: https://gateway.plastoboost.com/a/8?id= &tk=6j5l
+    message: "Api is currently freemeim for now!",
   });
 });
 
